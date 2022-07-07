733c9fb698908db4457ad73cf1c0ff9fd3acabe0
Michel Hoekstra <michel.h.hoekstra@gmail.com>
2022-07-07 16:04:08 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Michel Henrique Hoekstra on 07/07/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
